1e48a410249c879d3a043dbadb3988da1cbe4d81
Federico <38073413+Hexagan@users.noreply.github.com>
2025-06-24 01:29:19 -0300

    Acerca De y Home

--- app/src/main/java/ar/com/ifts18/istudent/AcercaDeActivity.kt
+++ app/src/main/java/ar/com/ifts18/istudent/AcercaDeActivity.kt
@@ -4,12 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.net.toUri
 
 class AcercaDeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acerca_de)
+
+
+        val btnTerminos = findViewById<Button>(R.id.buttonTerminos)
+
+        btnTerminos.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data =
+                "https://docs.github.com/en/site-policy/github-terms/github-terms-of-service".toUri()
+            startActivity(intent)
+        }
         val btnVolver = findViewById<Button>(R.id.buttonVolver)
 
         btnVolver.setOnClickListener {
